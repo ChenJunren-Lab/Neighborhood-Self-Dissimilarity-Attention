7cc6ff077eab0e3c3dcb4bef3a2727fa5db0fd52
陈俊任 <907409001@qq.com>
2025-09-22 06:29:42 +0800

    Update vit_seg_modeling.py

--- vit_seg_modeling.py
+++ vit_seg_modeling.py
@@ -357,7 +357,7 @@ class DecoderBlock(nn.Module):
         if (h//8)%2==0:
             x = NSDA(c, c, (h//8+1, w//8+1))(x)
         else:
-            x = NSDA(c, c, (h//8+2, w//8+2))(x)
+            x = NSDA(c, c, (h//8, w//8))(x)
 
         return x
 
