3af0ca6734c9ad84e9e78cf650f40c6af96c9b16
陈俊任 <907409001@qq.com>
2025-09-22 06:37:04 +0800

    Update vit_seg_modeling.py

--- vit_seg_modeling.py
+++ vit_seg_modeling.py
@@ -37,6 +37,7 @@ MLP_NORM = "LayerNorm_2"
 
 
 class NSDA(nn.Module):
+    # NSDA without Dynamic Neighborhood Scaling (DyNS)
     def __init__(self, in_channels, out_channels, window_size=(31, 31), shortcut=True, unbiased=True):
         super(NSDA, self).__init__()
 
